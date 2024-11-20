f004eecf503e9fc8f7a44c709628f05d610eeee3
zenos <zenos.chen@foxmail.com>
2024-11-20 22:02:56 +0800

    add sliding component

---
+++ src/components/SlidingComponent/Demo.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const listData = ref([
+  { name: "zenos 1" },
+  { name: "blue 1" },
+  { name: "zenos 2" },
+  { name: "blue 2" },
+]);
+
+const clickFn = () => {
+  console.log("hello world");
+};
+</script>
+
+<template>
+  <SlidingList :listData="listData" class="sliding-list">
+    <template #default="{ data }">
+      <div class="list-item list-item-2" @click="clickFn">{{ data.name }}</div>
+    </template>
+
+    <template #delete>
+      <div class="delete-slot" @click="clickFn">123</div>
+    </template>
+  </SlidingList>
+</template>
+
+<style>
+.sliding-list {
+  box-shadow: 0 0 4px 2px lightgray;
+  border-radius: 4px;
+}
+
+.delete-slot {
+  line-height: 60px;
+  width: 100%;
+}
+</style>
